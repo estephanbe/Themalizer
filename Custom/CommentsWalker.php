10aef77262f2f338227bbd590abb0d0369101b14
estephanbe <estephan.be@gmail.com>
2020-11-25 20:00:12 +0200

    update

--- Custom/CommentsWalker.php
+++ Custom/CommentsWalker.php
@@ -1,125 +1,120 @@
 <?php
-namespace BoshDev\Custom;
-
-
 
 /**
  * Main Nav Walker
  */
-class CommentsWalker extends \Walker_Comment
-{
-
-	function html5_comment( $comment, $depth, $args ) 
-    {
-        global $bod;
-        $tag = 'li';
-
-        $comment_author_url = get_comment_author_url( $comment );
-        $comment_author     = get_comment_author( $comment );
-        $avatar             = get_avatar( $comment, $args['avatar_size'] );
+class CommentsWalker extends \Walker_Comment {
 
 
-        $comment_timestamp = sprintf( __( '%1$s في %2$s', $bod->textDomain ), get_comment_date( '', $comment ), get_comment_time() );
+	function html5_comment( $comment, $depth, $args ) {
+		 global $bod;
+		$tag = 'li';
 
-        $comment_reply_link = get_comment_reply_link(
-                        array_merge(
-                            $args,
-                            array(
-                                'add_below' => 'div-comment',
-                                'depth'     => $depth,
-                                'max_depth' => $args['max_depth'],
-                                'reply_text' => 'رد<i class="licon-reply"></i>',
-                            )
-                        )
-                    );
+		$comment_author_url = get_comment_author_url( $comment );
+		$comment_author     = get_comment_author( $comment );
+		$avatar             = get_avatar( $comment, $args['avatar_size'] );
 
-        ?>
+		$comment_timestamp = sprintf( __( '%1$s في %2$s', $bod->textDomain ), get_comment_date( '', $comment ), get_comment_time() );
 
-        <<?php echo $tag; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( 'comment' ); ?>>
-        <article>
+		$comment_reply_link = get_comment_reply_link(
+			array_merge(
+				$args,
+				array(
+					'add_below'  => 'div-comment',
+					'depth'      => $depth,
+					'max_depth'  => $args['max_depth'],
+					'reply_text' => 'رد<i class="licon-reply"></i>',
+				)
+			)
+		);
 
-            <div class="gravatar">
+		?>
 
-                <?php
+		<<?php echo $tag; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( 'comment' ); ?>>
+		<article>
 
-                if ( 0 !== $args['avatar_size'] ) {
-                    if ( empty( $comment_author_url ) ) {
-                        echo wp_kses_post( $avatar );
-                    } else {
-                        printf( '<a href="%s">', $comment_author_url ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped --Escaped in https://developer.wordpress.org/reference/functions/get_comment_author_url/
-                        echo wp_kses_post( $avatar );
-                    }
-                }
+			<div class="gravatar">
 
-                if ( ! empty( $comment_author_url ) ) {
-                    echo '</a>';
-                }
+				<?php
 
-                ?>
+				if ( 0 !== $args['avatar_size'] ) {
+					if ( empty( $comment_author_url ) ) {
+						echo wp_kses_post( $avatar );
+					} else {
+						printf( '<a href="%s">', $comment_author_url ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped --Escaped in https://developer.wordpress.org/reference/functions/get_comment_author_url/
+						echo wp_kses_post( $avatar );
+					}
+				}
 
-                
-            </div>
+				if ( ! empty( $comment_author_url ) ) {
+					echo '</a>';
+				}
 
-            <div class="comment-body">
+				?>
 
-                <header class="comment-meta">
+				
+			</div>
 
-                    <h6 class="comment-author">
-                        <?php
+			<div class="comment-body">
 
-                        if ( !empty( $comment_author_url ) ) {
-                           
-                            printf( '<a href="%s">', $comment_author_url ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped --Escaped in https://developer.wordpress.org/reference/functions/get_comment_author_url/
-                        }
+				<header class="comment-meta">
 
-                        printf(
-                            '%1$s',
-                            esc_html( $comment_author )
-                        );
+					<h6 class="comment-author">
+						<?php
 
-                        if ( ! empty( $comment_author_url ) ) {
-                            echo '</a>';
-                        }
+						if ( ! empty( $comment_author_url ) ) {
 
-                        ?>
+							printf( '<a href="%s">', $comment_author_url ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped --Escaped in https://developer.wordpress.org/reference/functions/get_comment_author_url/
+						}
 
-                    </h6>
+						printf(
+							'%1$s',
+							esc_html( $comment_author )
+						);
 
-                    
+						if ( ! empty( $comment_author_url ) ) {
+							echo '</a>';
+						}
 
-                    <div class="comment-info">
-                        <time datetime="<?php comment_time( 'c' ); ?>" class="comment-date"><?php echo esc_attr( $comment_timestamp ); ?></time>
+						?>
 
-                        <?php
+					</h6>
 
-                        if ( $comment_reply_link ) {
-                            echo $comment_reply_link; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Link is escaped in https://developer.wordpress.org/reference/functions/get_comment_reply_link/
-                        }
+					
 
-                        ?>
+					<div class="comment-info">
+						<time datetime="<?php comment_time( 'c' ); ?>" class="comment-date"><?php echo esc_attr( $comment_timestamp ); ?></time>
 
-                        <!-- <a href="#" class="comment-reply-link"><i class="licon-reply"></i>Reply</a> -->
-                    </div>
+						<?php
 
-                </header>
+						if ( $comment_reply_link ) {
+							echo $comment_reply_link; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Link is escaped in https://developer.wordpress.org/reference/functions/get_comment_reply_link/
+						}
 
-                <p><?php comment_text(); ?></p>
+						?>
 
-            </div><!--/ .comment-body-->
+						<!-- <a href="#" class="comment-reply-link"><i class="licon-reply"></i>Reply</a> -->
+					</div>
+
+				</header>
+
+				<p><?php comment_text(); ?></p>
+
+			</div><!--/ .comment-body-->
 
 
 
-        </article>
+		</article>
 
 
 
 
 
 
-    
-    <?php 
-    echo "</$tag>";
-    }
+	
+		<?php
+		echo "</$tag>";
+	}
 }
 
 
--- themalizer-helper-functions.php
+++ themalizer-helper-functions.php
@@ -9,6 +9,7 @@
  * @license http://www.opensource.org/licenses/bsd-license.php BSD License
  */
 
+// phpcs:disable
 if ( ! defined( 'ABSPATH' ) ) {
 	exit( 'You are not allowed to get here, TINKY WINKY!!' ); // Exit if accessed directly.
 }
@@ -21,7 +22,7 @@ if ( ! function_exists( 'set_post_views' ) ) {
 	 * The query params to collect the posts is:
 	 * 'meta_key' => 'post_views_count',
 	 * 'orderby' => 'meta_value_num',
-	 * 
+	 *
 	 * @param integer $post_id the post id.
 	 * @return void
 	 */
@@ -39,3 +40,108 @@ if ( ! function_exists( 'set_post_views' ) ) {
 	}
 }
 
+if ( ! function_exists( 'the_breadcrumb' ) ) {
+	function the_breadcrumb() {
+		// $delimiter   = '&raquo;'; // delimiter between crumbs
+		$delimiter   = ''; // delimiter between crumbs
+		$home        = 'الرئيسية'; // text for the 'Home' link
+		$show_current = 1; // 1 - show current post/page title in breadcrumbs, 0 - don't show
+		$before      = '<li>'; // tag before the current crumb
+		$after       = '</li>'; // tag after the current crumb
+
+		global $post;
+		$home_link = get_bloginfo( 'url' );
+		echo '<li><a href="' . $home_link . '">' . $home . '</a></li> ' . $delimiter . ' ';
+		if ( is_category() ) {
+			$this_cat = get_category( get_query_var( 'cat' ), false );
+			if ( $this_cat->parent != 0 ) {
+				echo get_category_parents( $this_cat->parent, true, ' ' . $delimiter . ' ' );
+			}
+			echo $before . 'الأرشيف بحسب المواضيع "' . single_cat_title( '', false ) . '"' . $after;
+		} elseif ( is_search() ) {
+			echo $before . 'نتائج البحث لِ "' . get_search_query() . '"' . $after;
+		} elseif ( is_day() ) {
+			echo '<li><a href="' . get_year_link( get_the_time( 'Y' ) ) . '">' . get_the_time( 'Y' ) . '</a></li> ' . $delimiter . ' ';
+			echo '<li><a href="' . get_month_link( get_the_time( 'Y' ), get_the_time( 'm' ) ) . '">' . get_the_time( 'F' ) . '</a></li> ' . $delimiter . ' ';
+			echo $before . get_the_time( 'd' ) . $after;
+		} elseif ( is_month() ) {
+			echo '<li><a href="' . get_year_link( get_the_time( 'Y' ) ) . '">' . get_the_time( 'Y' ) . '</a></li> ' . $delimiter . ' ';
+			echo $before . get_the_time( 'F' ) . $after;
+		} elseif ( is_year() ) {
+			echo $before . get_the_time( 'Y' ) . $after;
+		} elseif ( is_single() && ! is_attachment() ) {
+			if ( get_post_type() != 'post' ) {
+				$post_type = get_post_type_object( get_post_type() );
+				$slug      = $post_type->rewrite;
+				echo '<li><a href="' . $home_link . '/' . $slug['slug'] . '/">' . $post_type->labels->singular_name . '</a></li>';
+				if ( $show_current == 1 ) {
+					echo ' ' . $delimiter . ' ' . $before . get_the_title() . $after;
+				}
+			} else {
+				$cat  = get_the_category();
+				$cat  = $cat[0];
+				$cats = get_category_parents( $cat, true, ' ' . $delimiter . ' ' );
+				if ( $show_current == 0 ) {
+					$cats = preg_replace( "#^(.+)\s$delimiter\s$#", '$1', $cats );
+				}
+				echo '<li>';
+				echo $cat->name !== 'Uncategorized' ? $cats : '';
+				echo '</li>';
+				if ( $show_current == 1 ) {
+					echo $before . get_the_title() . $after;
+				}
+			}
+		} elseif ( ! is_single() && ! is_page() && get_post_type() != 'post' && ! is_404() ) {
+			$post_type = get_post_type_object( get_post_type() );
+			echo $before . $post_type->labels->singular_name . $after;
+		} elseif ( is_attachment() ) {
+			$parent = get_post( $post->post_parent );
+			$cat    = get_the_category( $parent->ID );
+			$cat    = $cat[0];
+			echo get_category_parents( $cat, true, ' ' . $delimiter . ' ' );
+			echo '<li><a href="' . get_permalink( $parent ) . '">' . $parent->post_title . '</a></li>';
+			if ( $show_current == 1 ) {
+				echo ' ' . $delimiter . ' ' . $before . get_the_title() . $after;
+			}
+		} elseif ( is_page() && ! $post->post_parent ) {
+			if ( $show_current == 1 ) {
+				echo $before . get_the_title() . $after;
+			}
+		} elseif ( is_page() && $post->post_parent ) {
+			$parent_id   = $post->post_parent;
+			$breadcrumbs = array();
+			while ( $parent_id ) {
+				$page          = get_page( $parent_id );
+				$breadcrumbs[] = '<li><a href="' . get_permalink( $page->ID ) . '">' . get_the_title( $page->ID ) . '</a></li>';
+				$parent_id     = $page->post_parent;
+			}
+			$breadcrumbs = array_reverse( $breadcrumbs );
+			for ( $i = 0; $i < count( $breadcrumbs ); $i++ ) {
+				echo $breadcrumbs[ $i ];
+				if ( $i != count( $breadcrumbs ) - 1 ) {
+					echo ' ' . $delimiter . ' ';
+				}
+			}
+			if ( $show_current == 1 ) {
+				echo ' ' . $delimiter . ' ' . $before . get_the_title() . $after;
+			}
+		} elseif ( is_tag() ) {
+			echo $before . 'المقالات بحسب الكلمة المفتاحية  "' . single_tag_title( '', false ) . '"' . $after;
+		} elseif ( is_author() ) {
+			global $author;
+			$userdata = get_userdata( $author );
+			echo $before . 'المقالات التي نُشرت بواسطة ' . $userdata->display_name . $after;
+		} elseif ( is_404() ) {
+			echo $before . 'Error 404' . $after;
+		}
+		if ( get_query_var( 'paged' ) ) {
+			if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) {
+				echo ' (';
+			}
+			echo __( 'Page' ) . ' ' . get_query_var( 'paged' );
+			if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) {
+				echo ')';
+			}
+		}
+	} // end the_breadcrumb()
+}
